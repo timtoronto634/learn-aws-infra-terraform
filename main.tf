45cc396a85bc1ea20d3ee357379778ef994af29f
timtoronto634 <tim225x@gmail.com>
2022-06-28 07:46:11 +0900

    [add] enable ssh to ec2 instance

--- main.tf
+++ main.tf
@@ -76,6 +76,7 @@ resource "aws_instance" "main-ec2" {
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public_subnet.id
   key_name               = aws_key_pair.main-ec2-key-pair.id
+  vpc_security_group_ids = [aws_security_group.main-sg.id]
 
   tags = {
     Name = "main-ec2"
@@ -85,4 +86,51 @@ resource "aws_instance" "main-ec2" {
 resource "aws_key_pair" "main-ec2-key-pair" {
   key_name   = "test_key"
   public_key = file("./.ssh/${var.ec2_ssh_public_key}")
+}
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.main-ec2.id
+  vpc      = true
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_security_group" "main-sg" {
+  name        = "main-sg"
+  description = "test security_group"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_network_interface" "main-ni" {
+  subnet_id       = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.main-sg.id]
+
+  attachment {
+    instance     = aws_instance.main-ec2.id
+    device_index = 1
+  }
 }
