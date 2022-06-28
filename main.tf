674f919e72230864b2bac5013aff6236a789faec
timtoronto634 <tim225x@gmail.com>
2022-06-28 13:49:49 +0900

    [add] security rule
    can browse apche homepage

--- main.tf
+++ main.tf
@@ -108,11 +108,19 @@ resource "aws_security_group" "main-sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-  
+
   ingress {
     description      = "ssh"
     from_port        = 22
     to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "http"
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
